f9b4690e95e162930198a67a0571d8ebc3a6d804
Tshitenga Bahome <odettebahome@gmail.com>
2020-09-16 15:47:36 +0000

    Iterating Over Arrays

---
+++ packing-list.js
@@ -0,0 +1,6 @@
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensil"];
+console.log("Kitchen stuff to pack:");
+for (let i = 0; i < packingList.length; i++) {
+  console.log(packingList[i]);
+}
+console.log(packingList);
